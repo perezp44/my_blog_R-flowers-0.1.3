fbbe12e0ea1a6d597578d65cf731e9b88dd1a02c
pjperez <pedro.j.perez@uv.es>
2020-04-13 22:03:32 +0200

    todo a Github

--- _gestionando-blog.R
+++ _gestionando-blog.R
@@ -8,9 +8,21 @@ distill::create_post("Sending mails with R")
 git init
 
 #- vas a Github y creas un nuevo repo (sin Readme) y copias el link, entonces haces:
-git remote add origin https://github.com/perezp44/my_distill_web-blog.git  
+git remote add origin https://github.com/perezp44/my_blog_R-flowers-0.1.3.git
+  
+
 
 #- llevar a Git -------------------------------
 git add -A
 git commit --all --message "todo a Github"
 git push -u origin master
+
+
+#- traer de Git
+git pull origin master
+
+
+#- cambiar el origen
+git remote rm origin
+git remote add origin https://github.com/perezp44/my_blog_R-flowers-0.1.3.git
+
