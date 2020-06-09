9edff4f0694d599d32eeeeeb4bb867ecff45c30e
Pedro Perez <pedro.j.perez@uv.es>
2020-06-09 11:50:29 +0200

    Delete_tareas-a-hacer.R

--- _tareas-a-hacer.R
+++
@@ -1,6 +0,0 @@
-#- Tareas a hacer:
-
-#- 1) q se vean las figuras mas pequeñas
-#- 2) ver el workflow de los blogs de ejemplos
-#- 3) que en los slug de los post se vea resaltadao el `cozitas resaltadas`
-#- 4) A ver si puedes decorar los chunks con : https://kbodwin.github.io/flair/articles/how_to_flair.html
